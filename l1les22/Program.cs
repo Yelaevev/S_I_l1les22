9ebd513f7da2a687cb3bbc850800e4324cce210a
Yelaevev <yelaevevgen@gmail.com>
2020-08-10 17:38:11 +0300

    убрал повторения

--- l1les22/Program.cs
+++ l1les22/Program.cs
@@ -7,7 +7,8 @@ namespace Level1Space
     {
         public static string BalancedParentheses(int N)
         {
-            string ans1;/// добавляем замкнутые скобки +()+
+            int temp = N;
+            string ans1;/// добавляем замкнутые скобки + ()+
             if (N == 1) return "()";
             if (N == 2) return "()() (())";
             ans1 = BalancedParentheses(N - 1);
@@ -23,19 +24,20 @@ namespace Level1Space
 
                 i++;
             }
-
-            string ans3= BalancedParentheses(N - 1);
+            /// добавляем замкнутые скобки +() +
+            string ans3 = BalancedParentheses(N - 1);
             ans3 = ans3.Substring((N-1)*2+1);
             ans3 = ans3.Insert(ans3.Length, "()");
-            while (i < ans3.Length)
+            int l = 0;
+            while (l < ans3.Length)
             {
-                if (ans3[i] == ' ')
+                if (ans3[l] == ' ')
                 {
-                    ans3 = ans3.Insert(i, "()");
-                    i = i + 2;// добавили два элемента , чтобы в счете вернуться на прежнее место
+                    ans3 = ans3.Insert(l, "()");
+                    l = l + 2;// добавили два элемента , чтобы в счете вернуться на прежнее место
                 }
 
-                i++;
+                l++;
             }
 
             string ans2;/// добавляем обвалакивающие скобки (+ ......+) 
@@ -62,22 +64,62 @@ namespace Level1Space
             AnsTotal = AnsTotal.Insert(AnsTotal.Length, " ");
             AnsTotal = AnsTotal = AnsTotal.Insert(AnsTotal.Length, ans2);
             // ans = ans.Insert(0, ans);
+           
+
+            /////удаляем повторяющиеся комбинации из строки
+            string[] TotalArray = AnsTotal.Split(' ');
+            List<string> TotalList = new List<string>();
+            foreach (string t in TotalArray) TotalList.Add(t);
+
+            int Board = TotalList.Count;
+
+            int sample = 0;
+            while (sample < Board)
+            {
+                int check = 0;
+                while (check < Board)
+                {
+                   // if ((sample != check) && (string.Compare(TotalList[sample], TotalList[check]) == 1))
+                   if(TotalList[sample]== TotalList[check])
+                    {
+                        if (sample != check)
+                        {
+                            TotalList.RemoveAt(check);
+                            Board = Board - 1;
+                            check--;
+                        }
+                    
+                    }
+                    check++;
+                }
+                sample++;
+            }
+
+            AnsTotal = AnsTotal.Remove(0);
+            AnsTotal = TotalList[0];
+            for (int k = 1; k < TotalList.Count; k++)
+            {
+                AnsTotal = AnsTotal.Insert(AnsTotal.Length, " ");
+                AnsTotal = AnsTotal.Insert(AnsTotal.Length, TotalList[k]);
+            }
+           // Console.WriteLine(AnsTotal+" после");
             return AnsTotal;
         }
 
-        static void Main(string[] args)
-        {
-            //string t1 = BalancedParentheses(1);
-            //Console.WriteLine(t1);
-            //string t2 = BalancedParentheses(2);
-            //Console.WriteLine(t2);
-            string t3 = BalancedParentheses(3);
-            Console.WriteLine(t3);
+        //static void Main(string[] args)
+        //{
 
-            string t4 = BalancedParentheses(4);
-            Console.WriteLine(t4);
-            string t5 = BalancedParentheses(5);
-            Console.WriteLine(t5);
-        }
+        //    string t1 = BalancedParentheses(1);
+        //    Console.WriteLine(t1);
+        //    string t2 = BalancedParentheses(2);
+        //    Console.WriteLine(t2);
+        //    string t3 = BalancedParentheses(3);
+        //    Console.WriteLine(t3);
+
+        //    string t4 = BalancedParentheses(4);
+        //    Console.WriteLine(t4);
+        //    string t5 = BalancedParentheses(5);
+        //    Console.WriteLine(t5);
+        //}
     }
 }
